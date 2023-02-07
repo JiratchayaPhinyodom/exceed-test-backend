126b7c63fea67abed44cb765768e28affeab8bbe
JiratchayaPhinyodom <jiratchaya.phiny@ku.th>
2023-02-07 10:03:25 +0700

    Modified main.py

--- main.py
+++ main.py
@@ -1,52 +1,19 @@
-from fastapi import FastAPI, Body
-from typing import Union, Optional
-from pydantic import BaseModel
-
-
-class Item(BaseModel):
-    item_id : Union[int, str]
-    item_name: str = "   "
-    item_bool: bool
-
-class ItemDetail(BaseModel):
-    item_color: str
-    item_detail: str
-
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
-def root():
-    return {"msg": "welcome to root page"}
-
-@app.get("/items/1/foo/true")
-def show_item1():
-    return {"msg": "order"}
-
-@app.get("/items/{item_id}/{item_name}/{item_bool}")
-def show_item(item_id : Union[int, str], item_name: str, item_bool: bool):
-    return {"item_id": item_id, "item_name":item_name, "item_bool": item_bool}
-
-@app.post("/items", status_code=201)
-def create_item():
-    return {"msg": "created"}
-
-@app.get("/items")
-def query_item(item_id : int = 0, item_name: str = "   ", item_bool: Union[bool, None] = False):
-    if item_bool:
-        return {"item_id": item_id, "item_name":item_name}
-    return {"item_id": item_id, "item_name":item_name, "item_bool": item_bool}
-
-@app.get("/items/with_body")
-def show_item_body(item: Item, detail: ItemDetail):
-    # new_item_id = 's' + str(item.item_id)
-    return {"item": item, "detail": detail}
-
-@app.get("/items/with_body_params")
-def show_item_body_params(item: Item, item_color: str = Body()):
-    # new_item_id = 's' + str(item.item_id)
-    return {"item": item, "item_color": item_color}
-
-@app.get("/items/combine/{item_id}")
-def combine_all_params(item_id: str, item_name: str, item_detail: ItemDetail):
-    return {"item_id": item_id, "item_name": item_name, "item_detail": item_detail}
+def read_root():
+    return {"Hello": "World"}
